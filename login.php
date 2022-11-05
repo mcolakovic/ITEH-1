5df655fbee0ca50061b80efe25c6966e16a207d4
“Matija <mc20190132@student.fon.bg.ac.rs>
2022-11-05 13:18:08 +0100

    LogIn funkcionalnost izmijenjena

--- login.php
+++ login.php
@@ -23,7 +23,9 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
     <title>MILITARYSHOP - LogIn</title>
 
 </head>
@@ -32,21 +34,19 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     <div class="login-form">
         <div class="main-div">
             <form method="POST" action="#">
-                <div class="container">
-                    <label class="username">Korisnik</label>
-                    <input type="text" name="username" class="form-control" required>
-                    <br>
-                    <label for="password">Lozinka</label>
-                    <input type="password" name="password" class="form-control" required>
-                    <br>
-                    <button type="submit" class="btn btn-primary" name="submit">Prijavi se</button>
+                <h1>ULOGUJ SE</h1>
+                <div class="imgcontainer">
+                    <img src="img/military.png" id="military">
                 </div>
-
+                <div class="container">
+                    <input type="text" placeholder="Username" name="username" class="form-control" required>
+                    <br>
+                    <input type="password" placeholder="Password" name="password" class="form-control" required>
+                    <br>
+                    <button class="btn btn-success" name="submit" type="sumbit">Prijavi se</button>
+                </div>
             </form>
         </div>
-
-
-    </div>
 </body>
 
 </html>
